a73ab9a67f08fd4f445f1cb716677095302190c6
NatSantosMart <natsantosmartinez@gmail.com>
2024-03-25 19:19:59 +0100

    Navegación de "MainActivity" a nueva actividad "CreateGroup"

---
+++ app/src/main/java/com/spendTogether/CreateGroupActivity.kt
@@ -0,0 +1,16 @@
+package com.spendTogether
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CreateGroupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_create_group)
+
+    }
+}
