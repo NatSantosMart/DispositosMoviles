8f42c4c765c595a3c8b2325ef023ece91389a179
Adrian Hernandez <adrianhernandezmonterroso@gmail.com>
2024-04-14 19:04:27 +0200

    Estilado la vista de gastos

--- app/src/main/java/com/spendTogether/views/UsersMoneyViewHolder.kt
+++ app/src/main/java/com/spendTogether/views/UsersMoneyViewHolder.kt
@@ -5,6 +5,8 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.spendTogether.R
 import com.spendTogether.models.ExpenseResponse.ExpenseResponseItem
+import java.text.NumberFormat
+import java.util.Locale
 
 class UsersMoneyViewHolder (view: View): RecyclerView.ViewHolder(view) {
     private val expenseName: TextView = view.findViewById(R.id.expenseName)
@@ -20,9 +22,14 @@ class UsersMoneyViewHolder (view: View): RecyclerView.ViewHolder(view) {
     }
 
     fun bind(expense: ExpenseResponseItem) {
-        expenseUser.text = expense.paidBy
-        expenseMoney.text = expense.quantity
+        expenseUser.text = "Pagado por ${expense.paidBy}"
+        expenseMoney.text = formatCurrency(expense.quantity.toDouble())
         expenseDate.text = expense.date
         expenseName.text = expense.name
+    }
+
+    private fun formatCurrency(amount: Double): String {
+        val formatter = NumberFormat.getCurrencyInstance(Locale("es", "ES")) // Definir el formato de la moneda (euro)
+        return formatter.format(amount) // Formatear la cantidad con el símbolo del euro
     }
 }
